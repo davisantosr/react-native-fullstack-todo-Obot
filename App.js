335aa88f6825f9221321ca61e2dbc28585c6b4cc
davisantosr <davi-rocha2@hotmail.com>
2020-12-20 21:38:48 -0300

    components|style

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+        <TextInput
+          placeholder='Enter item'
+          style={styles.textInput}
+        />
+        <Button
+          title='add todo item'
+          onPress={() => console.log('Button clicked')}
+        />
+      </View>
+      <View>
+        <Text>Todo List</Text>
+      </View>
+      </View>
+      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#cecece',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 60
   },
+
+  textInput: {
+    padding: 10,
+    borderColor: '#000000',
+    marginBottom: 10,
+    borderWidth:1
+
+  }
 });
